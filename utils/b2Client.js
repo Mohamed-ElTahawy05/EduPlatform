753f11e17e3cf75761711d036af2c5b5c44985ec
moUA2007 <130896266+moUA2007@users.noreply.github.com>
2026-07-28 00:21:53 +0300

    fix: disable auto checksum calculation for presigned upload URLs

--- utils/b2Client.js
+++ utils/b2Client.js
@@ -2,11 +2,12 @@ const { S3Client } = require('@aws-sdk/client-s3');
 
 const b2Client = new S3Client({
     endpoint: process.env.B2_ENDPOINT,
-    region: 'us-west-005', // لازم يتطابق مع الـ region بتاع الـ endpoint عندك
+    region: 'us-east-005',
     credentials: {
         accessKeyId: process.env.B2_KEY_ID,
         secretAccessKey: process.env.B2_APPLICATION_KEY,
     },
+    requestChecksumCalculation: 'WHEN_REQUIRED',
 });
 
 module.exports = b2Client;
