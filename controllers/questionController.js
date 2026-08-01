8b440fe93beacb744ffd0999d1710a120937f4b0
moUA2007 <130896266+moUA2007@users.noreply.github.com>
2026-08-01 13:47:04 +0300

    feat: implement questionController

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,19 +1,24 @@
-exports.createQuestion = async (req, res) => {
+const Question = require('../models/Question');
+const Quiz = require('../models/Quiz');
+const factory = require('./handlerFactory');
+const catchAsync = require('../utils/catchAsync');
+const ApiError = require('../utils/ApiError');
 
-};
+exports.createQuestion = catchAsync(async (req, res, next) => {
+    const quiz = await Quiz.findById(req.body.quiz);
+    if (!quiz) {
+        return next(new ApiError('No quiz found with that ID', 404));
+    }
 
-exports.getAllQuestions = async (req, res) => {
+    const question = await Question.create(req.body);
 
-};
+    res.status(201).json({
+        status: 'success',
+        data: { data: question },
+    });
+});
 
-exports.getQuestion = async (req, res) => {
-
-};
-
-exports.updateQuestion = async (req, res) => {
-
-};
-
-exports.deleteQuestion = async (req, res) => {
-
-};
+exports.getAllQuestions = factory.getAll(Question);
+exports.getQuestion = factory.getOne(Question);
+exports.updateQuestion = factory.updateOne(Question);
+exports.deleteQuestion = factory.deleteOne(Question);
